9f7ebef1c4139b1076658f3ba9919c9b25427811
marcooegg <marco.oegg@gmail.com>
2021-05-11 16:48:31 -0300

    [ADD][subtickets module][Translation,actions,views]
    Incidence nbr./s.: ODOO-1274
    - Add parent_id and child_ids to ticket
    - Modified views
    - Removed Ticket_action
    - Add Translation

---
+++ helpdesk_sub_tickets/models/helpdesk_ticket.py
@@ -0,0 +1,31 @@
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+class HelpdeskTicket(models.Model):
+    _inherit = 'helpdesk.ticket'
+
+    parent_id = fields.Many2one(
+        'helpdesk.ticket',
+        string = 'Parent Ticket',
+        ondelete='restrict',
+        index = True
+    )
+    child_ids = fields.One2many(
+        'helpdesk.ticket',
+        'parent_id',
+        string = 'Child Tickets'
+    )
+
+    parent_title = fields.Char('Title', 
+    related = 'parent_id.name',
+    readonly= True)
+
+    
+    #prevents recursive relationships
+    @api.constrains('parent_id')
+    def _check_hierarchy(self):
+        if not self._check_recursion():
+            raise models.ValidationError(
+                'Error! You cannot create recursive tickets'
+            )
+    
